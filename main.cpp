5c60c60562282a3666e6e866f99cda81a9488600
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2023-12-29 14:52:19 -0600

    [ADD] simple exercise to manage math basics operations and print the results on terminal

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double x = 10;
+    double y = 5;
+
+    double z = (x  + 10) / (3 * y);
+
+    std::cout << "z = " << z << endl 
+         << "y = " << y;
+
+
+
+    return 0;
+}
